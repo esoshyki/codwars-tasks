62583baffe028abfd113ee4393f3d7545ef96cd6
esoshyki <esoshyki@yandex.by>
2019-04-03 10:41:59 +0300

    *solved 23 task*

---
+++ 23 Calculating with Functions/index.js
@@ -0,0 +1,15 @@
+function zero() {var val = 0; if (!arguments['0']) {return val} else { return Math.floor(eval(val + arguments['0'][1] + arguments['0'][0]))}}
+function one() {var val = 1; if (!arguments['0']) {return val} else { return Math.floor(eval(val + arguments['0'][1] + arguments['0'][0]))}}
+function two() {var val = 2; if (!arguments['0']) {return val} else { return Math.floor(eval(val + arguments['0'][1] + arguments['0'][0]))}}
+function three() {var val = 3; if (!arguments['0']) {return val} else { return Math.floor(eval(val + arguments['0'][1] + arguments['0'][0]))}}
+function four() {var val = 4; if (!arguments['0']) {return val} else { return Math.floor(eval(val + arguments['0'][1] + arguments['0'][0]))}}
+function five() {var val = 5; if (!arguments['0']) {return val} else { return Math.floor(eval(val + arguments['0'][1] + arguments['0'][0]))}}
+function six() {var val = 6; if (!arguments['0']) {return val} else { return Math.floor(eval(val + arguments['0'][1] + arguments['0'][0]))}}
+function seven() {var val = 7; if (!arguments['0']) {return val} else { return Math.floor(eval(val + arguments['0'][1] + arguments['0'][0]))}}
+function eight() {var val = 8; if (!arguments['0']) {return val} else { return Math.floor(eval(val + arguments['0'][1] + arguments['0'][0]))}}
+function nine() {var val = 9; if (!arguments['0']) {return val} else { return Math.floor(eval(val + arguments['0'][1] + arguments['0'][0]))}}
+
+function plus(x) {return [x, '+'] }
+function minus(x) {return [x, '-'] }
+function times(x) {return [x, '*'] }
+function dividedBy(x) {return [x, '/'] }
